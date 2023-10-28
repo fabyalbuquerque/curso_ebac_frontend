61b488b37d22f2027c35b323c7cb852c31390b00
fabliny-ab <fabliny.ab@gmail.com>
2023-10-28 12:07:22 -0300

    Acrescentando uma exceção

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 
-    const api = 'https://api.github.com/users/fabyalbuquerque';
+    const api = 'https://api.github.com/users/fabyalbuquerue';
 
     fetch(api)
     .then(function(resposta) {
+
+        if (resposta.status !==200) {
+            throw new Error('Solicitação falhou, verifique se o usuário existe.');
+        }
+
         return resposta.json();
     })
     .then(function(json) { 
@@ -14,5 +19,18 @@ $(document).ready(function() {
         $('#followers').find('span').text(json.followers);
         $('#following').find('span').text(json.following);
         $('#link').attr('href', json.html_url);
+    })
+    .catch(function(error) {
+        console.error(error);
+
+        $('#avatar').attr('src', "https://via.placeholder.com/180x180");
+        $('#name').text("Default");
+        $('#username').text("default");
+        $('#repository').find('span').text("N/A");
+        $('#followers').find('span').text("N/A");
+        $('#following').find('span').text("N/A");
+        $('#link').attr('href', "#");
+
+        alert("Perfil de usuário não encontrado");
     })
 })
