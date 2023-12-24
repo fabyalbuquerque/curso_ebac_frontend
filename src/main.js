1b46a7c73cc2f794914f546aaf25108dc85bf7c4
fabliny-ab <fabliny.ab@gmail.com>
2023-12-24 19:10:07 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// function multiplica(numero1: number, numero2: number): number {
+//     const resultado = numero1 * numero2;
+//     return resultado;
+// }
+// função 1
+const multiplica = (numero1, numero2) => numero1 * numero2;
+// função 2
+const saudacao = (nome) => "Olá " + nome; //ou `Olá ${nome}`
+console.log(multiplica(25, 10));
+console.log(saudacao("Fabliny"));
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// function multiplica(numero1: number, numero2: number): number {
+//     const resultado = numero1 * numero2;
+//     return resultado;
+// }
+// função 1
+const multiplica = (numero1: number, numero2: number): number => numero1 * numero2;
+
+// função 2
+const saudacao = (nome: string): string => "Olá " + nome; //ou `Olá ${nome}`
+
+console.log(multiplica(25, 10))
+console.log(saudacao("Fabliny"))
